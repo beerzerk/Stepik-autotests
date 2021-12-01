883aaa0f7e338090533f268f4421ea7d9731f5ef
Renat Kasimov <darka9e@gmail.com>
2021-12-01 22:07:05 +0100

    Added homeworks 2.3

---
+++ m2_l3_s6.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_css_selector("#answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+    time.sleep(1)
+
+
+finally:
+    time.sleep(10)
+    browser.quit()
+
